f0807fbe6ebfdbe3b5f1f47cb36bdeaf5cadf31f
CBT <CBT@CBT-2F-301>
2021-04-16 08:06:30 +0530

    version_0.2

---
+++ src/com_AcitTime_Test/demoGit.java
@@ -0,0 +1,14 @@
+package com_AcitTime_Test;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class demoGit
+{
+	@Test
+	public void testA()
+	{
+		Reporter.log("demo git prg....");
+	}
+
+}
